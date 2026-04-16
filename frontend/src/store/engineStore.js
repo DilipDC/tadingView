465252dfcb19138f93deb33c910505f67d2ebe08
Chethan <dilipdc.123dc@gmail.com>
2026-04-16 08:19:14 +0530

    Delete frontend/src/store directory

--- frontend/src/store/engineStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "./useStore";
-
-// engine running
-export const useEngineRunning = createStore("engineRunning", true);
-
-// engine status
-export const useEngineStatus = createStore("engineStatus", "running");
